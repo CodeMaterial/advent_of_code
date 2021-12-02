a4c66e94ffbf0ebaeecc5de704d09063884d0018
Samuel Martin <martinsa0182@gmail.com>
2021-12-02 09:56:11 +0000

    Small tweak to count_increases

--- dailyPuzzles/day1.py
+++ dailyPuzzles/day1.py
@@ -1,12 +1,8 @@
 import lib.helpers as helpers
 
 
-def count_increase(array_data):
-    c = 0
-    for d in array_data:
-        c += (d > 0)
-
-    return c
+def count_positive(array_data):
+    return sum(d > 0 for d in array_data)
 
 
 if __name__ == "__main__":
@@ -19,7 +15,7 @@ if __name__ == "__main__":
     for i in range(1, depths_count):
         depth_deltas[i] = depths[i] - depths[i-1]
 
-    print("raw depth increases: ", count_increase(depth_deltas))
+    print("raw depth increases: ", count_positive(depth_deltas))
 
     depth_deltas_smooth = [0] * depths_count
     sliding_window = 3
@@ -27,4 +23,4 @@ if __name__ == "__main__":
     for i in range(1, depths_count):
         depth_deltas_smooth[i] = sum(depths[i:i+sliding_window]) - sum(depths[i-1:i+sliding_window-1])
 
-    print("smooth depth increases: ", count_increase(depth_deltas_smooth))
+    print("smooth depth increases: ", count_positive(depth_deltas_smooth))
