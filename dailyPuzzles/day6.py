b4cffcc08335d34919ab4fb8e05f058e3fadc43d
Samuel Martin <martinsa0182@gmail.com>
2021-12-12 17:58:24 +0000

    compressed day 6

--- dailyPuzzles/day6.py
+++ dailyPuzzles/day6.py
@@ -2,20 +2,16 @@ import lib.helpers as helpers
 
 if __name__ == "__main__":
 
-    data = helpers.clean_input_data("../data/day6.txt")[0]
+    data = helpers.clean_input_data("../data/day6.txt")[0].split(",")
 
-    fish_ages = [0]*9
-    for f in data.split(","):
-        fish_ages[int(f)] += 1
+    fish_ages = [data.count(str(age)) for age in range(9)]
 
     for day in range(256):
 
-        at_zero = fish_ages[0]
+        fish_ages.append(fish_ages[0])
 
-        for i in range(len(fish_ages)-1):
-            fish_ages[i] = fish_ages[i+1]
+        fish_ages = fish_ages[1:]
 
-        fish_ages[8] = at_zero
-        fish_ages[6] += at_zero
+        fish_ages[6] += fish_ages[8]
 
     print("Puzzle input:", sum(fish_ages))
